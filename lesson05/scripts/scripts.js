6a134c9741a63aea78f67ae286a6727dab3c56d7
Schoolpro2022 <sakdispatch@gmail.com>
2024-05-24 00:01:21 -0600

    update

---
+++ lesson05/scripts/scripts.js
@@ -0,0 +1,33 @@
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+
+button.addEventListener('click', () => {
+    if (input.value !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+
+        deleteButton.setAttribute('aria-label', `Remove ${input.value}`);
+
+        li.appendChild(deleteButton);
+        list.appendChild(li);
+
+
+        deleteButton.addEventListener('click', () => {
+            list.removeChild(li);
+        });
+
+        input.value = '';
+        input.focus();
+    }
+    else {
+        alert('Please enter a chapter');
+        input.focus();
+    }
+});
