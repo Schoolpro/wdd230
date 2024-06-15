dcd6025db9fd914ee8102839208e7d687e3f5b94
Schoolpro2022 <sakdispatch@gmail.com>
2024-06-15 01:41:16 -0600

    asd

--- chamber/scripts/visit.js
+++ chamber/scripts/visit.js
@@ -1,19 +1,13 @@
-// Get the current date
 const currentVisit = Date.now();
 
-// Get the stored date of the last visit from localStorage, or set it to the current visit if it's the first visit
 const lastVisit = Number(window.localStorage.getItem("lastVisit")) || currentVisit;
 
-// Calculate the time difference in milliseconds
 const timeDifference = currentVisit - lastVisit;
 
-// Convert the time difference to days
 const daysBetweenVisits = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-// Get the visit message element
 const visitMessage = document.getElementById("visit-message");
 
-// Update the visit message based on the time between visits
 if (timeDifference === 0) {
     visitMessage.textContent = "Welcome! Let us know if you have any questions.";
 } else if (daysBetweenVisits < 1) {
@@ -24,5 +18,4 @@ if (timeDifference === 0) {
     visitMessage.textContent = `You last visited ${daysBetweenVisits} days ago.`;
 }
 
-// Store the current visit date in localStorage
 window.localStorage.setItem("lastVisit", currentVisit);
