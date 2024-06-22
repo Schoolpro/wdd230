ccfd79917e6d4c1813745c3475655970db5c5dc3
Schoolpro2022 <sakdispatch@gmail.com>
2024-06-21 23:08:17 -0600

    hwork

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,35 @@
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridButton.addEventListener("click", () => {
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listButton.addEventListener("click", showList);
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
+
+// Fetch member data from JSON file and display it
+fetch('data/members.json')
+    .then(response => response.json())
+    .then(data => {
+        const article = document.querySelector('article');
+        data.forEach(member => {
+            const section = document.createElement('section');
+            section.innerHTML = `
+                <img src="images/${member.image}" alt="${member.name}" />
+                <h3>${member.name}</h3>
+                <p>${member.address}</p>
+                <p>${member.phone}</p>
+                <a href="${member.website}" target="_blank">Website</a>
+                <p>${member.membershipLevel}</p>
+                <p>${member.description}</p>
+            `;
+            article.appendChild(section);
+        });
+    });
