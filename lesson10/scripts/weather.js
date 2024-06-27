3b2f7b8af0449f71e098a9063427bbc5b8f7986a
Schoolpro2022 <sakdispatch@gmail.com>
2024-06-27 17:39:07 -0600

    last update

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,22 +1,19 @@
-// Select HTML elements in the document
+
 const currentTemp = document.querySelector('#temperature');
 const weatherImage = document.querySelector('#weather-image');
 const description = document.querySelector('#description');
 
-// API key and location coordinates
-const apiKey = '4af53861742ef0628fb279558fe64705';
-const lat = 40.3649;
-const lon = -111.7382;
-const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}&lang=en`;
 
-// Asynchronous function to fetch the weather data
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=imperial&appid=4af53861742ef0628fb279558fe64705';
+
+
 async function apiFetch() {
     try {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data); // For testing purposes
-            displayResults(data); // Uncomment when ready
+            console.log(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -25,19 +22,17 @@ async function apiFetch() {
     }
 }
 
-// Function to display the results on the webpage
+
 function displayResults(data) {
-    currentTemp.textContent = `${Math.round(data.main.temp)} °C`;
+    currentTemp.textContent = `${Math.round(data.main.temp)} °F`;
     const iconSrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
     weatherImage.setAttribute('src', iconSrc);
     weatherImage.setAttribute('alt', data.weather[0].description);
     description.textContent = capitalizeWords(data.weather[0].description);
 }
 
-// Helper function to capitalize each word in the description
 function capitalizeWords(str) {
     return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
 
-// Invoke the apiFetch function
 apiFetch();
